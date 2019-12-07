17292e2c5bc8f9c8535f0892e5b17f8a5ae57557
pwolowski <pawel.wolowski@wp.pl>
2019-12-07 21:31:38 +0100

    Moj pierwszy program zapisany w Git

---
+++ MyFisrtJavaProject/src/ppj/git/Mail.java
@@ -0,0 +1,10 @@
+package ppj.git;
+
+public class Mail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First Java project with Git");
+	}
+
+}
